4e9d72c9e775f3f257a65ed59b778278b060adf1
GhulJr <oskarrek98@gmail.com>
2020-10-25 21:11:51 +0100

    feat: create fed fragment

---
+++ app/src/main/java/com/ghuljr/nasaclient/ui/main/search/FeedFragment.kt
@@ -0,0 +1,23 @@
+package com.ghuljr.nasaclient.ui.main.search
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ghuljr.nasaclient.R
+
+class FeedFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_feed, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) = FeedFragment()
+    }
+}
