ee24d54b28db29c468aa0e07ba4ae40c21634f93
GhulJr <oskarrek98@gmail.com>
2020-10-25 18:02:12 +0100

    feat: create splash screen

--- app/src/main/java/com/ghuljr/nasaclient/ui/main/MainActivity.kt
+++ app/src/main/java/com/ghuljr/nasaclient/ui/main/MainActivity.kt
@@ -1,10 +1,19 @@
 package com.ghuljr.nasaclient.ui.main
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ghuljr.nasaclient.R
+import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/ghuljr/nasaclient/ui/splash/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.ghuljr.nasaclient.ui.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ghuljr.nasaclient.R
+import com.ghuljr.nasaclient.ui.main.MainActivity
+import io.reactivex.Observable
+import java.util.concurrent.TimeUnit
+
+class SplashActivity : AppCompatActivity() {
+    @SuppressLint("CheckResult")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Observable.just(Intent(this, MainActivity::class.java))
+            .delay(1L, TimeUnit.SECONDS)
+            .subscribe {
+                startActivity(it)
+                finish()
+            }
+    }
+}
