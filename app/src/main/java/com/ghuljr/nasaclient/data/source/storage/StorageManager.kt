792adb963eef4587f6a1fbd1b0e59473587c6b82
GhulJr <oskarrek98@gmail.com>
2020-11-14 15:42:26 +0100

    Style: improve splash screen

--- app/src/main/java/com/ghuljr/nasaclient/data/source/storage/StorageManager.kt
+++ app/src/main/java/com/ghuljr/nasaclient/data/source/storage/StorageManager.kt
@@ -1,12 +1,10 @@
 package com.ghuljr.nasaclient.data.source.storage
 
-import android.util.Log
 import com.ghuljr.nasaclient.data.model.ApodModel
 import com.ghuljr.nasaclient.utils.dateToTimestamp
 import io.objectbox.Box
 import io.objectbox.rx.RxQuery
 import io.reactivex.Observable
-import io.reactivex.Single
 
 class StorageManager(private val apodBox: Box<ApodModel>) {
 
--- app/src/main/java/com/ghuljr/nasaclient/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/ghuljr/nasaclient/ui/splash/SplashActivity.kt
@@ -13,6 +13,7 @@ class SplashActivity : AppCompatActivity() {
     @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
 
         Observable.just(Intent(this, MainActivity::class.java))
             .delay(1L, TimeUnit.SECONDS)
