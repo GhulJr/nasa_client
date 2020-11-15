3efa903a049111c1c9b436f5dc5d23b91537702a
GhulJr <oskarrek98@gmail.com>
2020-11-15 16:43:15 +0100

    Feat: create apod recyclerview

---
+++ app/src/main/java/com/ghuljr/nasaclient/ui/main/ApodAdapter.kt
@@ -0,0 +1,34 @@
+package com.ghuljr.nasaclient.ui.main
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ghuljr.nasaclient.R
+import com.ghuljr.nasaclient.data.model.ApodModel
+import com.ghuljr.nasaclient.utils.loadImage
+import kotlinx.android.synthetic.main.item_apod.view.*
+
+class ApodAdapter : RecyclerView.Adapter<ApodAdapter.ViewHolder>() {
+
+    var apods: List<ApodModel> = listOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_apod, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(apods[position])
+
+    override fun getItemCount(): Int = apods.size
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(apod: ApodModel): Unit = with(itemView) {
+            apod_thumbnail.loadImage(apod.url)
+            apod_date.text = apod.date
+            apod_title.text = apod.date
+        }
+    }
+}
--- app/src/main/java/com/ghuljr/nasaclient/utils/PicassoExtention.kt
+++ app/src/main/java/com/ghuljr/nasaclient/utils/PicassoExtention.kt
@@ -3,6 +3,7 @@ package com.ghuljr.nasaclient.utils
 import android.widget.ImageView
 import com.squareup.picasso.Picasso
 
+//TODO: resize images
 fun ImageView.loadImage(url: String) {
     Picasso.get().load(url).into(this)
 }
